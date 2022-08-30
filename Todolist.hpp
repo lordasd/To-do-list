593c4e81e74e32c5626eb683e818167132de501a
David <101408842+lordasd@users.noreply.github.com>
2022-08-30 05:01:54 +0300

    Add files via upload

---
+++ Todolist.hpp
@@ -0,0 +1,23 @@
+#ifndef TODOLIST_HPP
+#define TODOLIST_HPP
+
+class Todolist {
+    std::vector<std::string> todo_list;
+    std::vector<bool> done_list;
+public:
+    void create_list(Todolist& list);
+    void add_to_list(Todolist& list);
+    void show_list(Todolist& list);
+    void list_view(Todolist& list);
+    void save_list(Todolist& list);
+    void delete_from_list(Todolist& list);
+    void delete_list(Todolist& list);
+    void load_save_file(Todolist& list);
+};
+
+enum Options{
+    CREATE_LIST = 1, ADD_TO_LIST, SHOW_LIST, SAVE_LIST,
+    DELETE_FROM_LIST, DELETE_LIST, EXIT
+};
+
+#endif //TODOLIST_HPP
