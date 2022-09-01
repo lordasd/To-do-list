a921fc52d8be37f6abdb7bba93195f3b297e323f
David <101408842+lordasd@users.noreply.github.com>
2022-09-01 07:31:39 +0300

    Update Todolist.hpp

--- Todolist.hpp
+++ Todolist.hpp
@@ -2,22 +2,37 @@
 #define TODOLIST_HPP
 
 class Todolist {
-    std::vector<std::string> todo_list;
-    std::vector<bool> done_list;
+    std::vector<std::vector<std::string>> todo_list;
+    std::vector<std::vector<bool>> done_list;
 public:
+    //Creates a new list and adds tasks
     void create_list(Todolist& list);
+    //Add a task to chosen list  
     void add_to_list(Todolist& list);
-    void add_task(Todolist& list);
-    void show_list(Todolist& list);
-    void list_view(Todolist& list);
+    //Add a task to a list(from 'add_to_list')
+    void add_task(std::vector<std::string>& tasks, std::vector<bool>& done);
+    //Shows all lists - choose a list to mark/umark(done/undone) tasks
+    void show_lists(Todolist& list);
+    //Mark/unmark(done/undone) tasks of a chosen list in "show_lists" 
+    void mark_tasks(std::vector<std::string>& tasklist, std::vector<bool>& donelist);
+    //Shows a list content
+    void list_view(const std::vector<std::string> tasks, const std::vector<bool> done);
+    //Shows all titles of lists
+    void title_lists_view(const std::vector<std::vector<std::string>>& lists);
+    //Saves the current lists to a file
     void save_list(Todolist& list);
+    //Loads up a lists from a file
     void load_list(Todolist& list);
+    //Deletes a task from a list
     void delete_from_list(Todolist& list);
+    //Deletes all lists/ a list
     void delete_list(Todolist& list);
+    //Checks if inputed index is available in the list(validity check)
+    bool index_in_list(const std::vector<std::vector<std::string>>& lists, size_t index);
 };
 
 enum Options{
-    CREATE_LIST = 1, ADD_TO_LIST, SHOW_LIST, SAVE_LIST,
+    CREATE_LIST = 1, ADD_TO_LIST, SHOW_LISTS, SAVE_LIST,
     LOAD_LIST, DELETE_FROM_LIST, DELETE_LIST, EXIT
 };
 
