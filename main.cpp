1b9fb71830ab902f70b3985b9a47f7872deb96d4
David <101408842+lordasd@users.noreply.github.com>
2022-09-02 15:45:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 #include <string>
 #include <chrono>
 #include <thread>
-#include "Todolist.hpp"
+#include "Todolist.h"
 
 int main()
 {
-    Todolist list;
+    ToDoLists lists;
 
     size_t input{};
-    while(true)
+    while (true)
     {
-        std::cout <<  " ____________\n";
+        std::cout << " ____________\n";
         std::cout << "| To-Do-List |\n";
-        std::cout <<  " ------------\n\n";
+        std::cout << " ------------\n\n";
         std::cout << "1) Create List\n2) Add to list\n3) Show list\n4) Save list\n";
         std::cout << "5) Load list\n6) Delete from list\n7) Delete list\n8) Exit\n\n";
         std::cout << "Option: ";
@@ -22,25 +22,25 @@ int main()
         switch (input)
         {
         case CREATE_LIST:
-            list.create_list(list);
+            create_list(lists);
             break;
         case ADD_TO_LIST:
-            list.add_to_list(list);
+            add_to_list(lists);
             break;
         case SHOW_LISTS:
-            list.show_lists(list);
+            show_lists(lists);
             break;
         case SAVE_LIST:
-            list.save_list(list);
+            save_list(lists);
             break;
         case LOAD_LIST:
-            list.load_list(list);
+            load_list(lists);
             break;
         case DELETE_FROM_LIST:
-            list.delete_from_list(list);
+            delete_from_list(lists);
             break;
         case DELETE_LIST:
-            list.delete_list(list);
+            delete_list(lists);
             break;
         case EXIT:
             exit(EXIT_SUCCESS);
